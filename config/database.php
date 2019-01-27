cf3637e2590b514c9f474f3afe5c0d3213b974c1
jacksonmcdaniel <jackson.mcdaniel78@gmail.com>
2019-01-27 14:40:05 -0800

    change database configuration for travis

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'statify'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', 'kOzdusTCgTszZjGZOQ6d'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -55,6 +55,19 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+
+        'testing' => [
+           'driver'     => 'mysql',
+           'host'       => env('DB_TEST_HOST', '127.0.0.1'),
+           'database'   => env('DB_TEST_DATABASE', 'statify_test'),
+           'username'   => env('DB_USERNAME', 'statify'),
+           'password'   => env('DB_PASSWORD', ''),
+           'charset'    => 'utf8mb4',
+           'collation'  => 'utf8mb4_unicode_ci',
+           'prefix'     => '',
+           'prefix_indexes' => true,
+           'strict'     => true,
+
 
         'pgsql' => [
             'driver' => 'pgsql',
