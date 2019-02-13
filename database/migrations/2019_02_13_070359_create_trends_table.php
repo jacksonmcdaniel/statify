48d7ee473e2ad3eb05ce7d9cf9146b1bb719afb7
Brian Ishii <brianlishii@gmail.com>
2019-02-12 23:21:34 -0800

    added new migrations for getting trends

--- database/migrations/2019_02_13_070359_create_trends_table.php
+++ database/migrations/2019_02_13_070359_create_trends_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTrendsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('uid');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('trends', function (Blueprint $table) {
+            $table->increments('tid');
+            $table->unsignedInteger('uid');
             $table->timestamps();
+
+            $table->foreign('uid')->references('uid')->on('users');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('trends');
     }
 }
---
+++ database/migrations/2019_02_13_071120_create_songs_in_trends_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsInTrendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs_in_trends', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('tid');
+            $table->unsignedInteger('sid');
+
+            $table->timestamps();
+
+            $table->foreign('tid')->references('tid')->on('trends');
+            $table->foreign('sid')->references('sid')->on('songs');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs_in_trends');
+    }
+}
