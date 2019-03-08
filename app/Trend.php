330d6dfcab5bde47e1f03eb1b3d01b59048effa9
Brian Ishii <brianlishii@gmail.com>
2019-03-07 17:45:59 -0800

    in order

--- app/Trend.php
+++ app/Trend.php
@@ -14,8 +14,9 @@ class Trend extends Model {
       $trend_id = DB::table('trends')->where('user_id', '=', $user_id)->where('trends.type', '=', $type)->first()->trend_id;
       $songs = DB::table('songs_in_trends')
          ->where('songs_in_trends.trend_id', '=', $trend_id)
-         ->join('songs', 'songs.song_id' ,'=', 'songs_in_trends.song_id')
+         ->join('songs', 'songs.song_id' ,'=', 'songs_in_trends.song_id')->orderBy('songs_in_trends.song_ordinal')
          ->select('songs.song_id', 'songs.song_name', 'songs.artist', 'songs_in_trends.song_ordinal')->get()->values();
+
       return $songs;
    }
 }
