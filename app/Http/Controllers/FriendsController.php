8cf364831d7d2b8888ca61b0fa297437ebeeb057
Brian Ishii <brianlishii@gmail.com>
2019-03-10 13:21:42 -0700

    friends works again

--- app/Http/Controllers/FriendsController.php
+++ app/Http/Controllers/FriendsController.php
@@ -12,14 +12,14 @@ class FriendsController extends Controller
 
 
         $friends = DB::table('friends')
-                ->join('users', 'friends.f_uid', '=', 'users.uid')
+                ->join('users', 'friends.f_uid', '=', 'users.user_id')
                 ->where('friends.uid', '=', $uid)
-                ->select('users.name', 'users.email', 'users.uid')
+                ->select('users.name', 'users.email', 'users.user_id')
                 ->get()->values();
 
         //dd($friends->toArray());
         return view('friends', [
-            'uid' => $uid,
+            'user_id' => $uid,
             'friends' => $friends,
             'tabIndex' => 0,
             'name' => "Friends"]);
