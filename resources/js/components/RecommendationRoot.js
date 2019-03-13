b286709d43df8b37ef854f67aa2afe9b6238c274
Shannon Marquiss <semarqui@calpoly.edu>
2019-03-13 13:27:06 -0700

    added snackbar to playlist button

--- resources/js/components/RecommendationRoot.js
+++ resources/js/components/RecommendationRoot.js
@@ -10,6 +10,8 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import Typography from '@material-ui/core/Typography';
 
 
 let id = 0;
@@ -22,19 +24,44 @@ export default class RecommendationRoot extends React.Component {
 
 	constructor(props) {
      super(props);
+     this.state = { open: false}
+
+     this.handleClick = state => () => {
+    this.setState({ open: true });
+  };
+
+  this.handleClose = () => {
+    this.setState({ open: false });
+  };
     }
 
     render() {
-        const { songs } = this.props;
+        const { songs} = this.props;
+        const { open } = this.state;
         return (
             <MuiThemeProvider theme={theme}>
               <Paper style={theme.paperContainer3}>
               
                 <StatifyTable songs={songs} style={{marginTop: "-200px"}}/>
                 <Grid container direction="column" alignItems="center" style={{padding: "50px"}}>
-                  <Button color="primary" variant="contained" href='/recommendations/playlist'>
+                  <Button color="primary" variant="contained" href='/recommendations/playlist' onClick={this.handleClick()}>
                     Create Spotify Playlist
                   </Button>
+                  <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    open={open}
+                    onClose={this.handleClose}
+                    ContentProps={{
+                      'aria-describedby': 'message-id',
+                      classes: {
+                        root: {
+                          background: 'primary'
+                          }
+                    }
+                    }}
+                    message={<span id="message-id">Spotify Playlist has been created</span>}
+                  />
+                  
                 </Grid>
                 
               </Paper>
