8c0d39edd01ad2457f9af8702bc8c1eea4c0f956
Brian Ishii <brianlishii@gmail.com>
2019-03-13 16:20:45 -0700

    fix home crashes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,17 +11,27 @@ class HomeController extends Controller
     public function index() {
         $user_id = session('user_id');
 
+        if ($user_id != null) {
+
         $topSong = Trend::getTopSong('long_term', $user_id);
         $topRecommendation = Recommendations::getTopSong($user_id);
 
+            return view('home', [
+                'user_id' => session('user_id'),
+                'tabIndex' => 0,
+                'topSong' => json_encode($topSong[0]),
+                'topArtist' => json_encode($topSong[0]),
+                'topRecommendation' => json_encode($topRecommendation[0]),
+                'name' => "Home"]);
+        }
+
         return view('home', [
-            'user_id' => session('user_id'),
+            'user_id' => null,
             'tabIndex' => 0,
-            'topSong' => json_encode($topSong[0]),
-            'topArtist' => json_encode($topSong[0]),
-            'topRecommendation' => json_encode($topRecommendation[0]),
+            'topSong' => null,
+            'topArtist' => null,
+            'topRecommendation' => null,
             'name' => "Home"]);
-            //'name' => "0};alert(0); var hi = {x:0"]);
     }
 
     public function logout() {
